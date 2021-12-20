53c1df3414efa5831675a6f2c8db0b0c28cc1781
BrunoRodriguesN <bruno_adeira@hotmail.com>
2021-12-20 16:47:59 -0300

    Exceção para CPF e EMAIL ja cadastrados

--- src/main/java/com/bruno/helpdesk/resources/exceptions/StandardError.java
+++ src/main/java/com/bruno/helpdesk/resources/exceptions/StandardError.java
@@ -2,20 +2,18 @@ package com.bruno.helpdesk.resources.exceptions;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable {	
+public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-	
-	
+
 	public StandardError() {
 		super();
 	}
-
 
 	public StandardError(Long timestamp, Integer status, String error, String message, String path) {
 		super();
@@ -26,62 +24,44 @@ public class StandardError implements Serializable {
 		this.path = path;
 	}
 
-
 	public Long getTimestamp() {
 		return timestamp;
 	}
-
 
 	public void setTimestamp(Long timestamp) {
 		this.timestamp = timestamp;
 	}
 
-
 	public Integer getStatus() {
 		return status;
 	}
-
 
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
 
-
 	public String getError() {
 		return error;
 	}
-
 
 	public void setError(String error) {
 		this.error = error;
 	}
 
-
 	public String getMessage() {
 		return message;
 	}
-
 
 	public void setMessage(String message) {
 		this.message = message;
 	}
 
-
 	public String getPath() {
 		return path;
 	}
 
-
 	public void setPath(String path) {
 		this.path = path;
 	}
-
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}	
-	
-	
-	
 
 }
--- src/main/java/com/bruno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/bruno/helpdesk/services/TecnicoService.java
@@ -6,8 +6,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bruno.helpdesk.domain.Pessoa;
 import com.bruno.helpdesk.domain.Tecnico;
 import com.bruno.helpdesk.domain.dtos.TecnicoDTO;
+import com.bruno.helpdesk.repositories.PessoaRepository;
 import com.bruno.helpdesk.repositories.TecnicoRepository;
 import com.bruno.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -16,6 +18,8 @@ public class TecnicoService {
 	
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -26,12 +30,24 @@ public class TecnicoService {
 		return repository.findAll();
 	}
 
-	public Tecnico create(TecnicoDTO objDTO) {
-	
+	public Tecnico create(TecnicoDTO objDTO) {	
 		objDTO.setId(null);
+		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 		
+	}
+
+	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+			if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+				throw new com.bruno.helpdesk.services.exceptions.DataIntegrityViolationException("CPF ja cadastrado no sistema!");
+			}
+			
+			obj = pessoaRepository.findByEmail(objDTO.getEmail());
+			if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+				throw new com.bruno.helpdesk.services.exceptions.DataIntegrityViolationException("EMAIL ja cadastrado no sistema!");
+			}
 	}
 
 }
